2e761605bbae72753ccebd00e9f264d08efafc37
MEDUZA <unkn.s0urc3@gmail.com>
2024-04-10 12:05:08 +0700

    Added initial code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <stack>
+
+template<typename T>
+class RealNumberStack  {
+private:
+    std::stack<T> stack;
+
+public:
+    void push(const T& value) {
+        stack.push(value);
+    }
+
+    void pop() {
+        if (!stack.empty()) {
+            stack.pop();
+        }
+    }
+
+    T top() {
+        if (!stack.empty()) {
+            return stack.top();
+        } else {
+            return T();
+        }
+    }
+
+    T bottom() {
+        if (!stack.empty()) {
+            std::stack<T> tempStack = stack;
+            while (tempStack.size() > 1) {
+                tempStack.pop();
+            }
+            return tempStack.top();
+        } else {
+            return T();
+        }
+    }
+
+    bool isEmpty() {
+        return stack.empty();
+    }
+
+    size_t size() {
+        return stack.size();
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    RealNumberStack <double> myStack;
+
+    myStack.push(3.14);
+    myStack.push(2.718);
+    myStack.push(1.618);
+
+    std::cout << "Top element: " << myStack.top() << std::endl;
+
+    std::cout << "Bottom element: " << myStack.bottom() << std::endl;
+
     return 0;
 }
